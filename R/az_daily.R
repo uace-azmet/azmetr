95f9c7d8943cd40a751bb4f38e02192800f81964
Eric R. Scott <scottericr@gmail.com>
2022-09-27 15:14:53 -0700

    finish up documentation

--- R/az_daily.R
+++ R/az_daily.R
@@ -6,20 +6,35 @@
 #'   stations will be returned
 #' @param start_date character; in YYYY-MM-DD or another format that can be
 #'   parsed by [lubridate::ymd()]
-#' @param end_date character; in YYYY-MM-DD in YYYY-MM-DD or another format that can be
-#'   parsed by [lubridate::ymd()].  Defaults to the current date if left blank.
-#' @details If neither `start_date` nor `end_date` are supplied, the most recent day of data will be returned.  If only `start_date` is supplied, then the end date defaults to the current date.  Supplying only `end_date` will result in an error.
+#' @param end_date character; in YYYY-MM-DD in YYYY-MM-DD or another format that
+#'   can be parsed by [lubridate::ymd()].  Defaults to the current date if left
+#'   blank.
+#' @details If neither `start_date` nor `end_date` are supplied, the most recent
+#'   day of data will be returned.  If only `start_date` is supplied, then the
+#'   end date defaults to the current date.  Supplying only `end_date` will
+#'   result in an error.
+#' @note If `station_id` is supplied as a vector, multiple successive calls to
+#'   the API will be made.  You may find better performance getting data for all
+#'   the stations by leaving `station_id` blank and subsetting the resulting
+#'   dataframe.
 #' @return a data frame
 #' @export
 #'
 #' @examples
+#' # Most recent data for all stations:
 #' az_daily()
-#' TODO: args should be start_date, end_date.  Do the parsing in the function I think.
-az_daily <- function(station_id = NULL, start_date = NULL, end_date = NULL, time_interval = NULL) {
+#'
+#' # Specify stations:
+#' az_daily(station_id = c(1, 2))
+#' az_daily(station_id = c("az01", "az02"))
+#'
+#' # Specify dates:
+#' az_daily(start_date = "2022-09-25")
+#' az_daily(start_date = "2022-09-25", end_date = "2022-09-26")
+#'
+az_daily <- function(station_id = NULL, start_date = NULL, end_date = NULL) {
 
   check_internet()
-
-  #validate and URL encode args
 
   if(!is.null(station_id)) {
     if(is.numeric(station_id)) {
