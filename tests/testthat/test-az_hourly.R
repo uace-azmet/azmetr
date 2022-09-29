3f82b686fae256d2621153a24df5232c8c9d8ec6
Eric R. Scott <scottericr@gmail.com>
2022-09-29 13:15:55 -0700

    set up casettes for tests

--- tests/testthat/test-az_hourly.R
+++ tests/testthat/test-az_hourly.R
@@ -1,17 +1,16 @@
 library(lubridate)
 dt <-
-  lubridate::now() |>
+  (lubridate::now() - days(1)) |>
   floor_date("hour") |>
   format("%Y-%m-%d %H")
 
 test_that("all stations return data", {
   skip_if_offline()
   skip_if_not(ping_service())
-  expect_equal(
-    az_hourly(start_date_time = dt, end_date_time = dt) |>
-      nrow(),
-    28
-  )
+  vcr::use_cassette("hourly", {
+    res <- az_hourly(start_date_time = dt, end_date_time = dt)
+  })
+  expect_equal(nrow(res), 28)
 })
 
 
@@ -20,29 +19,28 @@ test_that("start_date_time works as expected", {
   skip_if_not(ping_service())
   end <- floor_date(now(), "hour")
   start <- end - lubridate::hours(4)
-  expect_equal(
-    az_hourly(
+  vcr::use_cassette("hourly_start", {
+    res <- az_hourly(
       station_id = 1,
       start_date_time = format(start, "%Y/%m/%d %H"),
       end_date_time = format(end, "%Y/%m/%d %H")
-    ) |> nrow(),
-    5
-  )
+    )
+  })
+  expect_equal(nrow(res), 5)
 })
 
 
 test_that("works with station_id as a vector", {
-  expect_s3_class(az_hourly(station_id = c(1, 2)), "data.frame")
-  res <- az_hourly(station_id = c(1, 2), start_date_time = dt, end_date_time = dt)
+  vcr::use_cassette("hourly_station_vector", {
+    res <-
+      az_hourly(
+        station_id = c(1, 2),
+        start_date_time = dt,
+        end_date_time = dt
+      )
+  })
+  expect_s3_class(res, "data.frame")
   expect_equal(unique(res$meta_station_id), c("az01", "az02"))
-  expect_s3_class(
-    az_hourly(
-      station_id = c("az01", "az02"),
-      start_date_time = dt,
-      end_date_time = dt
-    ),
-    "data.frame"
-  )
 })
 
 #TODO: data tests (numeric? date?)
