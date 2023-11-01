0124d2615ceba5477988f7ecb539bff6f11c7026
Eric Scott <scottericr@gmail.com>
2023-11-01 16:40:25 -0400

    tests

--- tests/testthat/test-az_heat.R
+++ tests/testthat/test-az_heat.R
@@ -25,17 +25,23 @@ test_that("start and end dates interpreted correctly", {
     res <- az_heat(station_id = 1, start_date = "2023-10-10", end_date = "2023-10-30")
   })
   expect_equal(res$datetime_last, lubridate::ymd("2023-10-30"))
+
+
+  skip("not sure of desired behavior")
+  expect_error(az_heat(station_id = 1, end_date = "2022-09-01"), "`end_date` is before `start_date`!")
 })
 
 test_that("end_date can be specified without start_date", {
   skip_if_offline()
   skip_if_not(ping_service())
-  end <- "2022-09-27"
+  yesterday <- lubridate::today() - lubridate::days(1)
   with_mock_dir("heat_end", {
-    res_end <- az_heat(station_id = 1, end_date = end)
+    res_end_old <- az_heat(station_id = 1, end_date = "2022-09-27")
+    res_end_yesterday <- az_heat(station_id = 2, end_date = yesterday)
   })
-  expect_s3_class(res_end, "data.frame")
-  expect_equal(res_end$datetime_last, lubridate::ymd("2022-09-27"))
+  expect_s3_class(res_end_old, "data.frame")
+  expect_equal(res_end_old$datetime_last, lubridate::ymd("2022-09-27"))
+  expect_equal(res_end_yesterday$datetime_last, yesterday)
 })
 
 test_that("works with station_id as a vector", {
@@ -55,7 +61,7 @@ test_that("data is in correct format", {
     res_default <- az_heat()
   })
   expect_s3_class(res_default, "data.frame")
-  expect_equal(nrow(res_default), 28)
+  expect_equal(nrow(res_default), nrow(station_info))
   expect_type(res_default$meta_station_name, "character")
   expect_type(res_default$eto_pen_mon_in, "double")
   expect_s3_class(res_default$datetime_last, "Date")
