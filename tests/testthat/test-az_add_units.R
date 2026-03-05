21f460f3c8e10655693ec5cd7fcbf45c98f14fc3
Eric Scott <scottericr@gmail.com>
2026-03-05 15:22:07 -0700

    add to test

--- tests/testthat/test-az_add_units.R
+++ tests/testthat/test-az_add_units.R
@@ -6,13 +6,35 @@ test_that("all columns get assigned units that should", {
   res_daily <-
     az_daily(station_id = 1, start_date = "2023-11-27", end_date = "2023-11-29")
   res_hourly <-
-    az_hourly(station_id = 1, start_date_time = "2023-11-28 01", end_date_time = "2023-11-28 12")
+    az_hourly(
+      station_id = 1,
+      start_date_time = "2023-11-28 01",
+      end_date_time = "2023-11-28 12"
+    )
   res_heat <-
     az_heat(station_id = 1, end_date = "2023-11-28")
+  res_15min <- az_15min(
+    station_id = 1,
+    start = "2026-03-05 12:00:00",
+    end = "2026-03-05 13:00:00"
+  )
+  res_lw15 <- az_lw15min(
+    station_id = 1,
+    start = "2026-03-05 12:00:00",
+    end = "2026-03-05 13:00:00"
+  )
+  res_lwdaily <- az_lwdaily(
+    station_id = 1,
+    start = "2026-03-03",
+    end = "2026-03-04"
+  )
 
   heat_units <- az_add_units(res_heat)
   hourly_units <- az_add_units(res_hourly)
   daily_units <- az_add_units(res_daily)
+  min15_units <- az_add_units(res_15min)
+  lw15min_units <- az_add_units(res_lw15)
+  lwdaily_units <- az_add_units(res_lwdaily)
   expect_true(
     heat_units %>%
       dplyr::select(-starts_with("meta_"), -datetime_last) %>%
@@ -38,6 +60,37 @@ test_that("all columns get assigned units that should", {
         -wind_2min_timestamp
       ) %>%
       purrr::map_lgl(~inherits(.x, "units")) %>%
+      all()
+  )
+  expect_true(
+    min15_units %>%
+      dplyr::select(
+        -starts_with("meta_"),
+        -datetime,
+        -starts_with("date_"),
+      ) %>%
+      purrr::map_lgl(~ inherits(.x, "units")) %>%
+      all()
+  )
+  expect_true(
+    lw15min_units %>%
+      dplyr::select(
+        -starts_with("meta_"),
+        -datetime,
+        -starts_with("date_"),
+      ) %>%
+      purrr::map_lgl(~ inherits(.x, "units")) %>%
+      all()
+  )
+  expect_true(
+    lwdaily_units %>%
+      dplyr::select(
+        -starts_with("meta_"),
+        -date,
+        -datetime,
+        -starts_with("date_"),
+      ) %>%
+      purrr::map_lgl(~ inherits(.x, "units")) %>%
       all()
   )
 })
